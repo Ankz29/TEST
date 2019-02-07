60bd6803a4cfb29b481de09f9b1722c26d1d3bcd
Ankita <ankita.m@softwaysolutions.com>
2019-02-07 17:08:35 +0530

    SOurce code

---
+++ ClientValidation/Controllers/ValidationController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClientValidation.Models; 
+
+namespace ClientValidation.Controllers
+{
+    public class ValidationController : Controller
+    {
+        //
+        // GET: /Validation/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(Validation model)
+        {
+            if (ModelState.IsValid)
+            {
+                //bind the model attributes to send it to view//
+                ViewBag.Name = model.Name;
+                ViewBag.Phone = model.Phone;
+                ViewBag.Email = model.Email;
+                ViewBag.Gender = model.Gender; 
+                //clear the model once data is submitted// 
+                ModelState.Clear();
+
+                TempData["Success Message"] = "data saved successfully";
+
+                return View();
+
+
+                
+            }
+            return View(model); 
+            
+        }
+        
+	}
+}
---
+++ ClientValidation/Models/Validation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ClientValidation.Models
+{
+    public class Validation
+    {
+        //entered data validtions//
+       [Required(ErrorMessage = "Please enter Name.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter Phone No.")]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$",
+                   ErrorMessage = "Entered phone format is not valid.")]
+        public string Phone { get; set; }
+
+         [Required(ErrorMessage = "Please enter Phone No.")]
+         //[RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+
+         [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+                            @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                            @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
+                            ErrorMessage = "Email is not valid")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "- Select item -")]
+         public string Gender { get; set; }
+    }
+}
